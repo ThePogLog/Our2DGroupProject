413a8ae206ff3d5dd40f123f77e347fbbf8df03a
ThePogLog <punknman2007@gmail.com>
2024-10-08 10:29:43 -0700

    scene1 movement screipts

--- 2DGamesocool/Assets/Scene1/Scripts/PlayerHealth.cs
+++ 2DGamesocool/Assets/Scene1/Scripts/PlayerHealth.cs
@@ -6,6 +6,8 @@ public class PlayerHealth : MonoBehaviour
 {
     [SerializeField]
     int health = 10;
+    [SerializeField]
+    string leveltoload = Scene1;
     // Start is called before the first frame update
     void Start()
     {
@@ -36,8 +38,25 @@ public class PlayerHealth : MonoBehaviour
             health -= 1;
         }
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        Debug.Log(collision.gameObject.name);
+        //we want to take damage IF the player hits the enemy capsule
+        //bool key = true;
+        if (collision.gameObject.tag == "Enemy")
+        {
+            //health = health - 1;
+            health -= 1;
+            //health--;
+        }
     }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            health -= 1;
+        }
+    }
+
 }
