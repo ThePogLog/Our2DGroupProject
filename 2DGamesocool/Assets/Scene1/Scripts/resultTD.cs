19d723a7f0eb4282b2fbbd81911fad33b2325029
tiredJoblin <riley.lorenzo@gmail.com>
2024-10-22 10:29:08 -0700

    finished results detection code, and added graphics to the elements.

--- 2DGamesocool/Assets/Scene1/Scripts/resultTD.cs
+++ 2DGamesocool/Assets/Scene1/Scripts/resultTD.cs
@@ -10,6 +10,38 @@ public class resultTD : MonoBehaviour
     GameObject table2;
     [SerializeField]
     bool getCooken = false;
+    [SerializeField]
+    public bool fire = false;
+    [SerializeField]
+    public bool water = false;
+    [SerializeField]
+    public bool earth = false;
+    [SerializeField]
+    public bool wind = false;
+    [SerializeField]
+    public bool creatures = false;
+    [SerializeField]
+    public bool beasts = false;
+    [SerializeField]
+    public bool minerals = false;
+    [SerializeField]
+    public bool horrors = false;
+    [SerializeField]
+    public bool fire2 = false;
+    [SerializeField]
+    public bool water2 = false;
+    [SerializeField]
+    public bool earth2 = false;
+    [SerializeField]
+    public bool wind2 = false;
+    [SerializeField]
+    public bool creatures2 = false;
+    [SerializeField]
+    public bool beasts2 = false;
+    [SerializeField]
+    public bool minerals2 = false;
+    [SerializeField]
+    public bool horrors2 = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,8 +62,157 @@ public class resultTD : MonoBehaviour
 
         if (getCooken)
         {
+            if (table1.GetComponent<tableTD>().fire)
+            {
+                fire = true;
+            }
+            else
+            {
+                fire = false;
+            }
+            if (table1.GetComponent<tableTD>().water)
+            {
+                water = true;
+            }
+            else
+            {
+                water = false;
+            }
+            if (table1.GetComponent<tableTD>().wind)
+            {
+                wind = true;
+            }
+            else
+            {
+                wind = false;
+            }
+            if (table1.GetComponent<tableTD>().earth)
+            {
+                earth = true;
+            }
+            else
+            {
+                earth = false;
+            }
+            if (table1.GetComponent<tableTD>().creatures)
+            {
+               creatures = true;
+            }
+            else
+            {
+                creatures = false;
+            }
+            if (table1.GetComponent<tableTD>().beasts)
+            {
+                beasts = true;
+            }
+            else
+            {
+                beasts = false;
+            }
+            if (table1.GetComponent<tableTD>().minerals)
+            {
+                minerals = true;
+            }
+            else
+            {
+                minerals = false;
+            }
+            if (table1.GetComponent<tableTD>().horrors)
+            {
+                horrors = true;
+            }
+            else
+            {
+                horrors = false;
+            }
 
+            if (table2.GetComponent<tableTD2>().fire)
+            {
+                fire2 = true;
+            }
+            else
+            {
+                fire2 = false;
+            }
+            if (table2.GetComponent<tableTD2>().water)
+            {
+                water2 = true;
+            }
+            else
+            {
+                water2 = false;
+            }
+            if (table2.GetComponent<tableTD2>().wind)
+            {
+                wind2 = true;
+            }
+            else
+            {
+                wind2 = false;
+            }
+            if (table2.GetComponent<tableTD2>().earth)
+            {
+                earth2 = true;
+            }
+            else
+            {
+                earth2 = false;
+            }
+            if (table2.GetComponent<tableTD2>().creatures)
+            {
+                creatures2 = true;
+            }
+            else
+            {
+                creatures2 = false;
+            }
+            if (table2.GetComponent<tableTD2>().beasts)
+            {
+                beasts2 = true;
+            }
+            else
+            {
+                beasts2 = false;
+            }
+            if (table2.GetComponent<tableTD2>().minerals)
+            {
+                minerals2 = true;
+            }
+            else
+            {
+                minerals2 = false;
+            }
+            if (table2.GetComponent<tableTD2>().horrors)
+            {
+                horrors2 = true;
+            }
+            else
+            {
+                horrors2 = false;
+            }
         }
+        else
+        {
+            fire = false;
+            water = false;
+            earth = false;
+            wind = false;
+            creatures = false;
+            beasts = false;
+            minerals = false;
+            horrors = false;
+
+            fire2 = false;
+            water2 = false;
+            earth2 = false;
+            wind2 = false;
+            creatures2 = false;
+            beasts2 = false;
+            minerals2 = false;
+            horrors2 = false;
+        }
+
 
     }
 }
