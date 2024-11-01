702412e6f24065dec5d9fe4f938ed38ef3033e28
ThePogLog <punknman2007@gmail.com>
2024-11-01 10:10:17 -0700

    Final level updates

---
+++ 2DGamesocool/Assets/Scene1/Scripts/DestroyBullet1.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyBullet1 : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag != "EnemyBullets" && collision.gameObject.tag != "Enemy")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
+
