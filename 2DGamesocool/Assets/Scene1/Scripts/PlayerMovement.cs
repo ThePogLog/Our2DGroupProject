a24151cada23d41e29fda3c6032f104641125b39
klachag <26.kael.lachaga@ksd.org>
2024-11-05 08:13:23 -0800

    cabnet scrip animation fixed

--- 2DGamesocool/Assets/Scene1/Scripts/PlayerMovement.cs
+++ 2DGamesocool/Assets/Scene1/Scripts/PlayerMovement.cs
@@ -16,11 +16,8 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //check for horizontal and vertical input
-        //move the player based on that input
         float xInput = Input.GetAxis("Horizontal");
         float yInput = Input.GetAxis("Vertical");
-        //velocity is a Vector2 variable, storing 2 floats, x and y
         GetComponent<Rigidbody2D>().velocity = new Vector2 (xInput, yInput) * moveSpeed;
     }
 }
